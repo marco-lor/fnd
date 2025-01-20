bd570f2b784eda28877f9a6d5bfe9398aaf63900
marco-lor <f.mrc.lorenzo@gmail.com>
2025-01-20 20:20:39 +0100

    test incrocio backend frontend

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,24 @@ import json
 from fastapi import FastAPI
 import firebase_admin
 from firebase_admin import credentials, firestore
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "https://fatins.web.app",  # The Firebase Hosting URL of your frontend
+    "http://localhost:3000",        # For local development
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # The path to your secret file:
 service_account_path = "/etc/secrets/firestoreServiceAccountKey.json"
@@ -15,7 +31,7 @@ service_account_path = "/etc/secrets/firestoreServiceAccountKey.json"
 # Open the file and parse its JSON content:
 with open(service_account_path, "r") as f:
     service_account_info = json.load(f)
-print("FIREBASE_SERVICE_ACCOUNT:" + str(service_account_info))
+
 # cred = credentials.Certificate("firestoreServiceAccountKey.json")
 cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
