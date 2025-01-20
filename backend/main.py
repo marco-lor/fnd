d0cc5b40a4a753110f17eae834458155aca06ff4
marco-lor <f.mrc.lorenzo@gmail.com>
2025-01-20 20:02:37 +0100

    prova lettura secret file

--- backend/main.py
+++ backend/main.py
@@ -7,13 +7,14 @@ from fastapi import FastAPI
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+app = FastAPI()
+
 # The path to your secret file:
 service_account_path = "/etc/secrets/firestoreServiceAccountKey.json"
 
 # Open the file and parse its JSON content:
 with open(service_account_path, "r") as f:
     service_account_info = json.load(f)
-
 print("FIREBASE_SERVICE_ACCOUNT:" + str(service_account_info))
 # cred = credentials.Certificate("firestoreServiceAccountKey.json")
 cred = credentials.Certificate(service_account_info)
