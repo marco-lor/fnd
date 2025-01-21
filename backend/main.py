614fe3c973dbc76e9ac0c733fbff41f0bf339ed5
marco-lor <f.mrc.lorenzo@gmail.com>
2025-01-21 02:30:21 +0100

    aggiunto anche https://fatins.firebaseapp.com/ come indirizzo raggiungibile per FastAPI

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ app = FastAPI()
 
 origins = [
     "https://fatins.web.app",  # The Firebase Hosting URL of your frontend
+    "https://fatins.firebaseapp.com/", # The Firebase Hosting URL of your frontend #2
     "http://localhost:3000",   # For local development
 ]
 
